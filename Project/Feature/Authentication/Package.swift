23e3ae3694b56eb28680870f99d2bdfd5065a0bb
Scott Moon <interactord@gmail.com>
2024-03-31 17:28:55 +0900

    feat: Core, Feature 베이스 추가

---
+++ Project/Feature/Authentication/Package.swift
@@ -0,0 +1,30 @@
+// swift-tools-version: 5.10
+
+import PackageDescription
+
+let package = Package(
+  name: "Authentication",
+  platforms: [ .iOS(.v17) ],
+  products: [
+    .library(
+      name: "Authentication",
+      targets: ["Authentication"]),
+  ],
+  dependencies: [
+    .package(path: "../Core/Domain"),
+    .package(path: "../Core/Platform"),
+    .package(path: "../Core/Functor"),
+  ],
+  targets: [
+    .target(
+      name: "Authentication",
+      dependencies: [
+        "Domain",
+        "Platform",
+        "Functor",
+      ]),
+    .testTarget(
+      name: "AuthenticationTests",
+      dependencies: ["Authentication"]),
+  ]
+)
