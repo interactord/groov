8061bcf6bacf2106d0b9b56ea013d7179171e125
Scott Moon <interactord@gmail.com>
2024-03-31 16:10:58 +0900

    Swift Auto Lint & Format

---
+++ Package.swift
@@ -0,0 +1,13 @@
+// swift-tools-version: 5.10
+
+import PackageDescription
+
+let package = Package(
+  name: "groov",
+  products: [
+  ],
+  dependencies: [
+    .package(url: "https://github.com/airbnb/swift", from: "1.0.5"),
+  ],
+  targets: [
+  ])
